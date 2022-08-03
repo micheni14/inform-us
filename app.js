f3a6ccd63d37f58bd2db84a4e10cc2fc656e0332
micheni14 <lewismichen42@gmail.com>
2022-08-03 17:53:51 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let title=document.querySelector('.title');
+let content=document.querySelector('.content');
+title.addEventListener("click",()=>{
+    content.classList.toogle('content-active');
+}
+);
+
